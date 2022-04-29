c15114f0ecfcbed6f19205837761692fcbee3f3d
JanPLosa <jplosa@tlsi.es>
2022-04-29 10:55:20 +0200

    Reaccionar a los cambios en tiempo real

--- src/app/shared/directives/err-msg.directive.ts
+++ src/app/shared/directives/err-msg.directive.ts
@@ -1,9 +1,9 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[error-msg]'
 })
-export class ErrMsgDirective implements OnInit{
+export class ErrMsgDirective implements OnInit, OnChanges{
 
   htmlElement: ElementRef<HTMLElement>;
   @Input() color: string = 'red';
@@ -14,6 +14,20 @@ export class ErrMsgDirective implements OnInit{
     this.htmlElement = el;
 
    }
+  ngOnChanges(changes: SimpleChanges): void {
+
+    if( changes['mensaje'] ) {
+      const mensaje = changes['mensaje'].currentValue;
+      this.htmlElement.nativeElement.innerText = mensaje;
+    }
+
+    if( changes['color'] ) {
+      const color = changes['color'].currentValue;
+      this.htmlElement.nativeElement.style.color = this.color;
+    } 
+
+    console.log(changes);
+  }
   ngOnInit(): void {
     console.log('NgOnInit directiva');
     this.setColor();
